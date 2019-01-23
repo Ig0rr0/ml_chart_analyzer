8ad3f31b0f3c82b26f0268ce12777ea06610adfe
Igorro <190rr0@gmail.com>
2019-01-23 22:12:41 +0200

    * use dataprovider in tests

--- tests/Controller/Api/ChartControllerTest.php
+++ tests/Controller/Api/ChartControllerTest.php
@@ -11,12 +11,15 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class ChartControllerTest extends WebTestCase
 {
-    public function testApiReturns()
+	/**
+	 * @dataProvider provideData
+	 * @param $to_test_data
+	 */
+    public function testApiReturns($to_test_data)
     {
         $client = static::createClient();
 
         $to_test = ChartDto::$necessary_params;
-        $to_test_data = ['https://eth.nanopool.org/api/v1/price_history/0/768', 'Ethereum price ', '$.data.*.time', '$.data.*.price', 'Time period', 'Price value', '20'];
         $parameters = [];
         foreach ($to_test as $test_var_id => $test_var) {
             $parameters['import_data'][$test_var] = $to_test_data[$test_var_id];
@@ -29,4 +32,16 @@ class ChartControllerTest extends WebTestCase
         self::assertNotFalse($contents = \json_decode($response->getContent(), true), 'failed json decode API');
         self::assertGreaterThan(2, $contents);
     }
+
+	public function provideData()
+	{
+		return [[
+			[
+				'https://eth.nanopool.org/api/v1/price_history/0/768',
+				'Ethereum price ',
+				'$.data.*.time', '$.data.*.price',
+				'Time period', 'Price value',
+				'20']
+		]];
+	}
 }
